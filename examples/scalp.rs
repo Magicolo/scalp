6ed952c2a868cb2acd2fa98977ec8bf9c247f86d
Magicolo <magicololand@gmail.com>
2024-02-24 17:17:46 +0000

    Removed test code.

--- examples/scalp.rs
+++ examples/scalp.rs
@@ -4,8 +4,6 @@ use std::error;
 fn main() -> Result<(), Box<dyn error::Error>> {
     let result = Parser::builder()
         .pipe(header!())
-        .summary("A paosif o soif osi soiaoooais po apoa aposi paois fpasofi fpofi fo. aposi paois fpasofi fpofi fo aposi paois fpasofi fpofi fo aposi paois fpasofi fpofi fo")
-        .summary("A paosif o soif osi soiaoooais po apoa aposi paois fpasofi fpofi fo.")
         .usage("Usage: scalp [OPTIONS]")
         .verb(|verb| verb.name("b").name("boba").help("Throws a Boba."))
         .verb(|verb| verb.name("f").name("fett").help("Catches a Fett."))
