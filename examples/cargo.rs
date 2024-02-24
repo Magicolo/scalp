3d6e54b379883aa2c511685c7765dd6f17657f68
Magicolo <magicololand@gmail.com>
2024-02-24 17:19:07 +0000

    clean up

--- examples/cargo.rs
+++ examples/cargo.rs
@@ -3,12 +3,5 @@ use scalp::*;
 pub struct Cargo {}
 
 fn main() -> Result<(), Error> {
-    // let parser = Builder::new()
-    //     .name("git")
-    //     .version(env!("CARGO_PKG_VERSION"))
-    //     .map(|()| Cargo {})
-    //     .build()?;
-    // let arguments = ["--help"];
-    // let environment = [("", "")];
     Ok(())
 }
--- examples/git.rs
+++ examples/git.rs
@@ -3,12 +3,5 @@ use scalp::*;
 pub struct Git {}
 
 fn main() -> Result<(), Error> {
-    // let parser = Builder::new()
-    //     .name("git")
-    //     .version(env!("CARGO_PKG_VERSION"))
-    //     .map(|()| Git {})
-    //     .build()?;
-    // let arguments = ["--help"];
-    // let environment = [("", "")];
     Ok(())
 }
